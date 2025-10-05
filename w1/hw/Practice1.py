c2e67f392c3143fb3e2a391a28040b546703686c
SedMehdiAlavi <alavisedmehdi@gmail.com>
2025-10-05 19:18:03 +0330

    comments removed

--- w1/hw/Practice1.py
+++ w1/hw/Practice1.py
@@ -15,19 +15,3 @@ while True:
     reverse_name = name [::-1]
     print(reverse_name)
 
-#  for vowel in vowels
-# print(name)
-# for
-# new_name: str = name.replace(" ", "")
-# new_name: str = new_name.replace("e", ".")
-# new_name: str = new_name.replace("a", ".")
-# new_name: str = new_name.replace("i", ".")
-# new_name: str = new_name.replace("o", ".")
-# new_name: str = new_name.replace("u", ".")
-# new_name: str = new_name.replace("E", ".")
-# new_name: str = new_name.replace("A", ".")
-# new_name: str = new_name.replace("I", ".")
-# new_name: str = new_name.replace("O", ".")
-# new_name: str = new_name.replace("U", ".")
-# reverse_name: str = new_name [::-1]
-# print(reverse_name)
